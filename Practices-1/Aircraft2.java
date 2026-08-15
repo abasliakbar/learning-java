c5a69a09beef9c7966d8ececcaa94ed422fd7e6b
abasovf <abaslikbr@gmail.com>
2026-08-15 12:48:45 +0400

    second commit

--- Practices-1/Aircraft2.java
+++
@@ -1,6 +0,0 @@
-public class Aircraft2 {
-    String model;
-    String manufacturer;
-    int maxSpeed;
-    Pilot pilot;
-}
--- Practices-1/App.java
+++
@@ -1,8 +0,0 @@
-public class App {
-    public static void main(String[] args) {
-        Author author = new Author("Akbar");
-        Book book = new Book("Dark reader", author);
-        System.out.println(author);
-
-    }
-}
